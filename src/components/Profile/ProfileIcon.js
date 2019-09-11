def9da0fd17affbeea22f62a62fd95a22e09346e
Personal <personal@Gavins-MacBook-Pro.local>
2019-09-10 21:06:02 -0600

    added dropdown stuff

--- src/components/Profile/ProfileIcon.js
+++ src/components/Profile/ProfileIcon.js
@@ -1,14 +1,43 @@
-import React, { useState } from "react";
+import React, { useState, Fragment } from "react";
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
 
-const ProfileIcon = () => {
-  const [dropdownOpen, setDropdownOpen] = useState();
+const ProfileIcon = ({ onRouteChange }) => {
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
   return (
-    <div class="pa4 tc">
-      <img
-        src="http://tachyons.io/img/logo.jpg"
-        class="br-100 ba h3 w3 dib"
-        alt="avatar"
-      />
+    <div className="pa4 tc">
+      <Dropdown
+        style={{ marginRight: "30px" }}
+        direction="up"
+        isOpen={dropdownOpen}
+        toggle={() => setDropdownOpen(!dropdownOpen)}
+      >
+        <DropdownToggle
+          tag="span"
+          data-toggle="dropdown"
+          aria-expanded={dropdownOpen}
+        >
+          <img
+            src="http://tachyons.io/img/logo.jpg"
+            className="br-100 ba h3 w3 dib"
+            alt="avatar"
+          />
+        </DropdownToggle>
+        <DropdownMenu
+          className="b--transparent shadow-5"
+          style={{ backgroundColor: "rgba(255,255,255,0.5" }}
+        >
+          <DropdownItem>View Profile</DropdownItem>
+          <DropdownItem onClick={() => onRouteChange("signout")}>
+            Sign Out
+          </DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
     </div>
   );
 };
